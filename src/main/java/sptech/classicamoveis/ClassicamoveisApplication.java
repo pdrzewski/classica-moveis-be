0f3bc1aac4712a891588e462ff72a1a1827076f3
pdrzewski <pedro.martins@sptech.school>
2026-07-07 19:04:22 -0300

    Base do projeto

---
+++ src/main/java/sptech/classicamoveis/ClassicamoveisApplication.java
@@ -0,0 +1,13 @@
+package sptech.classicamoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicamoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicamoveisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sptech/classicamoveis/ClassicamoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package sptech.classicamoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicamoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
