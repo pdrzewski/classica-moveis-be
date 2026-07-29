b39efebb98cea281865fe67cd3b35939dc692d64
pdrzewski <pedro.martins@sptech.school>
2026-07-29 19:15:49 -0300

    feat: adiciona estrutura do mapper

--- src/main/java/sptech/classicamoveis/Produto/mapper/ProdutoMapper.java
+++ src/main/java/sptech/classicamoveis/Produto/mapper/ProdutoMapper.java
@@ -1,4 +1,42 @@
 package sptech.classicamoveis.Produto.mapper;
 
+import org.springframework.stereotype.Component;
+import sptech.classicamoveis.Produto.model.Produto;
+import sptech.classicamoveis.Produto.dto.ProdutoRequestDTO;
+import sptech.classicamoveis.Produto.dto.ProdutoResponseDTO;
+
+@Component
 public class ProdutoMapper {
+
+    public ProdutoResponseDTO toResponseDTO(Produto produto) {
+        if (produto == null) {
+            return null;
+        }
+        return new ProdutoResponseDTO(
+                produto.getNome(),
+                produto.getFornecedor(),
+                produto.getCategoria(),
+                produto.getPrecoCusto(),
+                produto.getEstoqueMin(),
+                produto.getNcm()
+        );
+    }
+
+    public Produto toEntity(ProdutoRequestDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+        Produto produto = new Produto();
+        preencherEntidade(produto, dto);
+        return produto;
+    }
+
+    public void preencherEntidade(Produto produto, ProdutoRequestDTO dto) {
+        produto.setNome(dto.nome());
+        produto.setFornecedor(dto.fornecedor());
+        produto.setCategoria(dto.categoria());
+        produto.setPrecoCusto(dto.precoCusto());
+        produto.setEstoqueMin(dto.estoqueMin());
+        produto.setNcm(dto.ncm());
+    }
 }
